0bff37fe86e0184c9bab4a644ada3cdc8b83d50a
AVS-Origami <avs.origami@gmail.com>
2023-04-06 11:08:56 -0400

    Restart again

--- build_env/build_scripts/packages/gettext.sh
+++
@@ -1,9 +0,0 @@
-. /dist/build_env/build_start $1 $0
-
-./configure --disable-shared
-
-make
-
-cp -v gettext-tools/src/{msgfmt,msgmerge,xgettext} /usr/bin
-
-. /dist/build_env/build_end $1
--- build_env/build_scripts/packages/python.sh
+++
@@ -1,9 +0,0 @@
-. /dist/build_env/build_start $1 $0
-
-./configure --prefix=/usr   \
-            --enable-shared \
-            --without-ensurepip
-
-make && make install
-
-. /dist/build_env/build_end $1

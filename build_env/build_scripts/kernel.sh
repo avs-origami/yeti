164e63a99f82059013b79bc7ee32d9ef9330e73d
AVS-Origami <avs.origami@gmail.com>
2023-04-12 21:59:08 -0400

    Install kernel

--- build_env/build_scripts/kernel.sh
+++ build_env/build_scripts/kernel.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 . build_start $1 $0
 
+cp $DIST_ROOT/files/linux.config .config
+sed -i ".config" -e 's,^CONFIG_CROSS_COMPILE=.*,CONFIG_CROSS_COMPILE="'$TARGET'-",'
 
+make -j$(nproc) ARCH="arm" CROSS_COMPILE="${TARGET}-" oldconfig
+make -j$(nproc) ARCH="arm" CROSS_COMPILE="${TARGET}-" zImage dtbs modules
+
+mkdir -pv $CLFS/sysroot/boot
+cp arch/arm/boot/zImage "$CLFS/sysroot/boot/zImage"
+cp -r arch/arm/boot/dts "$CLFS/sysroot/boot"
+
+make ARCH="arm" CROSS_COMPILE="${TARGET}-" INSTALL_MOD_PATH="$CLFS/sysroot" modules_install
 
 cd $DIST_ROOT/build_env
 . build_end $1
